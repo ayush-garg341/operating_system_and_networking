08265347dd8bf3f00a5f7c5f99bd07628ddb948a
Ayush Garg <gargayush341@gmail.com>
2024-05-14 10:15:16 +0530

    concurrent linked list but not much scalable

---
+++ c/virtualization/locked_data_structures/linked_list/linked_list_non_scalable.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../common.h"
+#include "../common_threads.h"
+
+typedef struct __node_t {
+    int key;
+    struct __node_t *next;
+} node_t;
+
+typedef struct __list_t {
+    node_t *head;
+    pthread_mutex_t lock;
+} list_t;
+
+void list_init(list_t *l){
+    l->head = NULL;
+    pthread_mutex_init(&l->lock, NULL);
+}
+
+int list_insert(list_t *l, int key){
+    Pthread_mutex_lock(&l->lock);
+    node_t *new_node = malloc(sizeof(node_t));
+    if (new_node == NULL){
+        perror("malloc");
+        pthread_mutex_unlock(&l->lock);
+        return -1;
+    }
+    new_node->key = key;
+    new_node->next = l->head;
+    l->head = new_node;
+    Pthread_mutex_unlock(&l->lock);
+    return 0;
+}
+
+int list_lookup(list_t *l, int key)
+{
+    Pthread_mutex_lock(&l->lock);
+    node_t *current = l->head;
+    while (current != NULL) {
+        if (current->key == key){
+            Pthread_mutex_unlock(&l->lock);
+            return 0;
+        }
+        current = current->next;
+    }
+    Pthread_mutex_unlock(&l->lock);
+    return -1;
+}
+
+int main(int argc, char *argv[])
+{
+    list_t *l = malloc(sizeof(list_t));
+    list_init(l);
+    printf("%d\n", list_insert(l, 5));
+    printf("%d\n", list_insert(l, 3));
+    printf("%d\n", list_insert(l, 8));
+    printf("%d\n", list_lookup(l, 3));
+}
